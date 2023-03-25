d70245a0e45e02f654d193170da20a21e326b77b
lostmart <lostmart@hotmail.com>
2023-03-25 13:00:16 +0100

    all test green

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -140,7 +140,7 @@ export default class {
 	}
 
 	handleShowTickets(e, bills, index) {
-		console.log(bills)
+		// console.log(bills)
 		if (this.counter === undefined || this.index !== index) this.counter = 0
 		if (this.index === undefined || this.index !== index) this.index = index
 
